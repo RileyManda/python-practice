ecde557c8364d0978c8fd5bdb27af259fae50db4
rileymanda1@gmail.com <rileymanda1@gmail.com>
2021-01-16 21:17:30 -0800

    improved UI Layout-Text Sizes

--- madlibs/ui.py
+++ madlibs/ui.py
@@ -5,10 +5,10 @@ sg.theme('DarkTanBlue')
 # Very basic window.
 
 layout = [
-    [sg.Text('Adjective',key='adj-')],
-    [sg.Text('Verb',key='verb1'), sg.InputText()],
-    [sg.Text('Verb',key='verb2'), sg.InputText()],
-    [sg.Text('Famous Person:',key='person'), sg.InputText()],
+    [sg.Text('Adjective',key='adj-',size=(12,2)),sg.InputText()],
+    [sg.Text('Verb1: ',key='verb1',size=(12,2)), sg.InputText()],
+    [sg.Text('Verb2: ',key='verb2',size=(12,2)), sg.InputText()],
+    [sg.Text('Person:',key='famous_person',size=(12,2)), sg.InputText()],
     [sg.Submit(), sg.Cancel(), sg.Button('Reset')],
 ]
 
@@ -16,5 +16,5 @@ window = sg.Window('Alien Madlibs', layout)
 event, values = window.read()
 window.close()
 #output
-print(event,  f"Computer programming is so", values[0], "It makes me so excited all the time \
-    I love to",values[1],"Stay hydrated and","like you are ",values[2])
+print(event,  f"Computer programming is so", values[0], "It makes me so excited all the time",
+    "I love to",values[1],"Stay hydrated and",values[2],"like you are",values[3])
