64d92cd4457ef4b1af9a97b202fec00e42b51b0f
rileymanda1@gmail.com <rileymanda1@gmail.com>
2021-01-16 16:59:35 -0800

    completed simple madlibs game

--- madlibs/madlibs.py
+++ madlibs/madlibs.py
@@ -1,6 +1,11 @@
 #beginning of the madlibs game code in python
-
+# get Input
 adj = input("Adjective: ")
-
-madlib = f"Computer programming is so{adj}! It makes me so excited all the time \
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+gamename = f"MadLibs"
+famous_person = input ("Famous Person: ")
+print("Welcome to{}".format(gamename))
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time \
     I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+print(madlib)
